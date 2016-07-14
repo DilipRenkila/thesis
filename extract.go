05ba003b0ac1aa21b26229d21a3dc300b6796310
diliprenkila <dilip.renkila278@gmail.com>
2016-07-14 12:50:05 +0200

    refined extract.go

--- extract.go
+++ extract.go
@@ -25,9 +25,39 @@ func read_file (path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+func append_file(lines []string) error {
+
+	file, err := os.OpenFile("results.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+    		return err
+	}
+	defer file.Close()
+
+	if _, err = file.WriteString(lines); err != nil {
+    		return err
+	}
+
+	return err
+}
+
+
 func main() {
 
 	lines ,err := read_file("/logs/trace-7474-1.txt")
+	if err != nil {
+		log.Fatalf("read_file: %s",err)
+	}
+	delay,err := read_file("/delay.txt")
+	if err != nil {
+		log.Fatalf("read_file: %s",err)
+	}
+
+	expid,err := read_file("/expid.txt")
+	if err != nil {
+		log.Fatalf("read_file: %s",err)
+	}
+
+	runid,err := read_file("/runid.txt")
 	if err != nil {
 		log.Fatalf("read_file: %s",err)
 	}
@@ -64,5 +94,9 @@ func main() {
 	}
 	x := float64(len(lines_d01))
 	fmt.Println(average_delay / x)
+	err = append_file(fmt.Sprintf("expid:%s runid:%s delay:%s average_delay:%f",expid,runid,delay,average_delay))
+	if err != nil {
+		log.Fatalf("append_file: %s",err)
+	}
 
 }
