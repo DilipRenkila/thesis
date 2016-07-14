1f9524f741a60d0737d90cd171f0b8ba0e6d6e28
Dilip Renkila <DilipRenkila@users.noreply.github.com>
2016-07-14 12:21:17 +0200

    refined extract.go

--- extract.go
+++ extract.go
@@ -10,14 +10,27 @@ import (
 	"strconv"
 )
 
-
-func main() {
-	pwd, _ := os.Getwd()
-	file, err := os.Open(pwd + "/logs/trace-7474-1.txt")
+func read_file (path string) ([]string, error) {
+	pwd , _ := os.Getwd()
+	file, err := os.Open(pwd + path)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer file.Close()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+
+	lines ,err := read_file("/logs/trace-7474-1.txt")
+	if err != nil {
+		log.Fatalf("read_file: %s",err)
+	}
 
 	re_1,err := regexp.Compile(`d01`)
 	re_2,err := regexp.Compile(`d10`)
@@ -29,36 +42,27 @@ func main() {
 
 	var lines_d01 []string
 	var lines_d10 []string
-	//var average_delay []float64
 	average_delay := 0.0
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		if re_1.MatchString(scanner.Text()) == true  {
-		lines_d01 =  append(lines_d01,scanner.Text())
+
+	for _,line := range lines {
+		if re_1.MatchString(line) == true  {
+		lines_d01 =  append(lines_d01,line)
 		}
 
-		if re_2.MatchString(scanner.Text()) == true {
-		lines_d10 = append(lines_d10,scanner.Text())
+		if re_2.MatchString(line) == true {
+		lines_d10 = append(lines_d10,line)
 		}
-
 	}
-	//fmt.Println(lines_d01)
+
 	for i, _ := range lines_d01 {
-		//fmt.Println(i, lines_d01[i])
 		in := strings.Split(lines_d01[i], ":")
 		out := strings.Split(lines_d10[i], ":")
 		In, _ := strconv.ParseFloat(in[3], 64)
 		Out,_ := strconv.ParseFloat(out[3], 64)
 		delay := Out - In
 		average_delay = average_delay + delay
-	//	fmt.Println(i,in[3],out[3],delay)
 	}
 	x := float64(len(lines_d01))
 	fmt.Println(average_delay / x)
-	//fmt.Println(len(lines_d01),len(lines_d10))
 
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
 }
