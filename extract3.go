8deb289ebb4b7a17dd83fda49a5d86dcc3f88591
diliprenkila <dilip.renkila278@gmail.com>
2016-07-15 21:14:20 +0200

    final extract2.go

---
+++ extract3.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"strings"
+	"fmt"
+)
+func main() {
+	line := "[1790]:d10:mp10165:7263.868126398500:LINK(1070):CAPLEN( 256): IPv4: UDP: 192.168.186.251:37717 --> 192.168.186.221:1500 len=1036 check=63034"
+	in := strings.Split(line, "=")
+	fmt.Println(in[2])
+}
