98005e145d08eab09b6106f31434380a01619544
diliprenkila <dilip.renkila278@gmail.com>
2016-07-15 19:28:41 +0200

    trace file -> text file = extract2.go

--- extract2.go
+++ extract2.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"log"
 )
 
 func printCommand(cmd *exec.Cmd) {
@@ -17,21 +18,40 @@ func printError(err error) {
   }
 }
 
-func printOutput(outs []byte) {
+func printOutput(outs []byte,filename string) {
   if len(outs) > 0 {
     fmt.Printf("==> Output: %s\n", string(outs))
+	  pwd := "/home/ats/dire15/thesis/logs"
+	  file, err := os.OpenFile(
+        pwd + "filename",
+        os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+        0666,
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    // Write bytes to file
+
+    bytesWritten, err := file.Write(string(outs))
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("Wrote %d bytes.\n", bytesWritten)
+
   }
 }
 func main() {
 	expid :=7482 ; runid := 1
 	//converting tracefile to a text file
 	tracefile := fmt.Sprintf("/mnt/LONTAS/traces/trace-%d-%d.cap",expid,runid)
-	//tracedestiny := fmt.Sprintf("/home/ats/dire15/thesis/logs/trace-%d-%d.txt",expid,runid)
+	tracedestiny := fmt.Sprintf("/trace-%d-%d.txt",expid,runid)
 	cmd :=exec.Command("capshow",tracefile)
 	// Create an *exec.Cmd for executing os commands
 	// Combine stdout and stderr
 	printCommand(cmd)
 	output, err := cmd.CombinedOutput()
 	printError(err)
-	printOutput(output)
+	printOutput(output,tracedestiny)
 }
