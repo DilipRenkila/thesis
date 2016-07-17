8de7649f626bcf7a40ad724e12b21611f563cc74
diliprenkila <dilip.renkila278@gmail.com>
2016-07-17 19:27:39 +0200

    extract3.go

--- extract.go
+++ extract.go
@@ -106,11 +106,12 @@ func main() {
 
 	lines ,err := read_file(fmt.Sprintf("/logs/trace-%s-%s.txt",expid[0],runid[0]))
 	if err != nil {
-		log.Fatalf("read_file: %s",err)
+		log.Fatalf("read_file: %s", err)
 	}
 
-	re_1,err := regexp.Compile(`d01`)
-	re_2,err := regexp.Compile(`d10`)
+	re_1, err := regexp.Compile(`d01`)
+	re, err := regexp.Compile(`UDP`)
+	re_2, err := regexp.Compile(`d10`)
 
 	if err != nil {
 		fmt.Printf("There is a problem with your regexp.\n")
@@ -120,30 +121,55 @@ func main() {
 	var lines_d01 []string
 	var lines_d10 []string
 	average_delay := 0.0
+	number_of_packets := 0
 
-	for _,line := range lines {
-		if re_1.MatchString(line) == true  {
-		lines_d01 =  append(lines_d01,line)
+	for _, line := range lines {
+		if re_1.MatchString(line) == true && re.MatchString(line) == true {
+			lines_d01 = append(lines_d01, line)
 		}
 
-		if re_2.MatchString(line) == true {
-		lines_d10 = append(lines_d10,line)
+		if re_2.MatchString(line) == true && re.MatchString(line) == true {
+			lines_d10 = append(lines_d10, line)
 		}
 	}
+	m := make(map[string]float64)
 
 	for i, _ := range lines_d01 {
 		in := strings.Split(lines_d01[i], ":")
-		out := strings.Split(lines_d10[i], ":")
+		d01_checksum_string := strings.Split(lines_d01[i], "=")
 		In, _ := strconv.ParseFloat(in[3], 64)
-		fmt.Println(out)
-		Out,_ := strconv.ParseFloat(out[3], 64)
-		delay := Out - In
-		average_delay = average_delay + delay
+		//fmt.Println(d01_checksum_string)
+		checksum := d01_checksum_string[2]
+		m[checksum] = In
+
 	}
-	x := float64(len(lines_d01))
+	fmt.Println(len(lines_d01),len(lines_d10))
+	for j, _ := range lines_d10 {
+		out := strings.Split(lines_d10[j], ":")
+		d10_checksum_string := strings.Split(lines_d10[j], "=")
+		//fmt.Println(d10_checksum_string)
+
+		checksum := d10_checksum_string[2]
+		Out, _ := strconv.ParseFloat(out[3], 64)
+		if _, ok := m[checksum]; ok {
+			number_of_packets = number_of_packets + 1
+			In := m[checksum]
+			delay := In - Out
+			average_delay = average_delay + delay
+
+        	} else {
+                	fmt.Println("key not found")
+        	}
+
+	}
+
+	x := float64(number_of_packets)
 	average_delay = average_delay/x
-	fmt.Println(average_delay)
-	err = append_file(fmt.Sprintf("expid:%s runid:%s delay:%s average_delay:%f\n",expid[0],runid[0],delay[0],average_delay))
+	drop := len(lines_d01) - len(lines_d10)
+	fmt.Println(average_delay*1000,drop)
+
+
+	err = append_file(fmt.Sprintf("expid:%s runid:%s delay:%s average_delay:%f dropped_packets:%d\n",expid[0],runid[0],delay[0],average_delay,drop))
 	if err != nil {
 		log.Fatalf("append_file: %s",err)
 	}
