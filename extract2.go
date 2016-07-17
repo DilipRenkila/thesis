761baa2234965706ac96cbd0937738db0340a479
diliprenkila <dilip.renkila278@gmail.com>
2016-07-17 21:48:28 +0200

    extract3.go

--- extract2.go
+++ extract2.go
@@ -113,6 +113,7 @@ func extract() {
 
 	re_1, err := regexp.Compile(`d01`)
 	re, err := regexp.Compile(`UDP`)
+	reg, err := regexp.Compile(`packets read.`)
 	re_2, err := regexp.Compile(`d10`)
 
 	if err != nil {
@@ -126,11 +127,11 @@ func extract() {
 	number_of_packets := 0
 
 	for _, line := range lines {
-		if re_1.MatchString(line) == true && re.MatchString(line) == true {
+		if re_1.MatchString(line) == true && re.MatchString(line) == true && reg.MatchString(line )== false{
 			lines_d01 = append(lines_d01, line)
 		}
 
-		if re_2.MatchString(line) == true && re.MatchString(line) == true {
+		if re_2.MatchString(line) == true && re.MatchString(line) == true && reg.MatchString(line)== false{
 			lines_d10 = append(lines_d10, line)
 		}
 	}
