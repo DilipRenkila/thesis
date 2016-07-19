456b128855995a88bffd5848834645a4472f602c
diliprenkila <dilip.renkila278@gmail.com>
2016-07-20 01:00:48 +0200

    new go package

--- delayfinder/capshow.sh
+++ delayfinder/capshow.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-capshow /mnt/LONTAS/tracesle/trace-7518-1.cap >> /mnt/LONTAS/ExpControl/dire15/logs/trace-7518-1.txt
+capshow /mnt/LONTAS/traces/trace-7518-1.cap >> /mnt/LONTAS/ExpControl/dire15/logs/trace-7518-1.txt
