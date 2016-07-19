267cffee1f62193d1f5e2e00e2772ddfac34c1bc
diliprenkila <dilip.renkila278@gmail.com>
2016-07-19 19:55:04 +0200

    config.pl is changed

--- config.pl
+++ config.pl
@@ -1,19 +1,32 @@
 #!/usr/bin/perl
- $ip1="10.1.0.127"; # 10.1.0.127
- $p1=161;
- $c1="public";
- $ip2="10.1.0.123"; # 10.1.0.123
- $p2=161;
- $c2="public";
- $OID_sysUpTime = '1.3.6.1.2.1.1.3.0';
- $in1="1.3.6.1.2.1.2.2.1.10.1";
- $out1="1.3.6.1.2.1.2.2.1.16.1";
- $in8="1.3.6.1.2.1.2.2.1.10.8";
- $out8="1.3.6.1.2.1.2.2.1.16.8";
- $IN1="1.3.6.1.2.1.2.2.1.10.1";
- $OUT1="1.3.6.1.2.1.2.2.1.16.1";
- $IN2="1.3.6.1.2.1.2.2.1.10.2";
- $OUT2="1.3.6.1.2.1.2.2.1.16.2";
- @oid1 =($OID_sysUpTime,$in1,$out1,$in8,$out8);
- @oid2 =($OID_sysUpTime,$IN1,$OUT1,$IN2,$OUT2); 
+
+use strict;
+use warnings;
+ my $ip1="10.1.0.127"; # 10.1.0.127
+ my $p1=161;
+ my $c1="public";
+ my $ip2="10.1.0.123"; # 10.1.0.123
+ my $p2=161;
+ my $c2="public";
+ my $OID_sysUpTime = '1.3.6.1.2.1.1.3.0';
+ my $in1="1.3.6.1.2.1.2.2.1.10.1";
+ my $out1="1.3.6.1.2.1.2.2.1.16.1";
+ my $in8="1.3.6.1.2.1.2.2.1.10.8";
+ my $out8="1.3.6.1.2.1.2.2.1.16.8";
+ my $IN1="1.3.6.1.2.1.2.2.1.10.1";
+ my $OUT1="1.3.6.1.2.1.2.2.1.16.1";
+ my $IN2="1.3.6.1.2.1.2.2.1.10.2";
+ my $OUT2="1.3.6.1.2.1.2.2.1.16.2";
+ my @oid1 =($OID_sysUpTime,$in1,$out1,$in8,$out8);
+ my @oid2 =($OID_sysUpTime,$IN1,$OUT1,$IN2,$OUT2);
+
+open (FOO,"/mnt/LONTAS/ExpControl/dire15/info/details.txt") ||
+    die "ERROR Unable to open file: $!\n";
+
+my $last;
+my $first = <FOO>;
+
+while (<FOO>) { $last = $_ }
+close FOO;
+print "$last\n";
 
---
+++ poller.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Net::SNMP qw(snmp_dispatcher ticks_to_time);
+use Time::HiRes qw(usleep gettimeofday);
+use JSON;
+my $script="/home/ats/dire15/thesis/config.pl";
+do "$script";
+my ($second,$microsecond)=gettimeofday;
+open FILE1, ">>",""
