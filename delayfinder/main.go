ac83539ba46875baf3e95d6cb247c7cf63687768
diliprenkila <dilip.renkila278@gmail.com>
2016-07-19 21:35:42 +0200

    new go package

---
+++ delayfinder/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "log"
+    "golang.org/x/exp/inotify"
+)
+
+func main() {
+    watcher, err := inotify.NewWatcher()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    done := make(chan bool)
+
+    // Process events
+    go func() {
+        for {
+            select {
+            case ev := <-watcher.Event:
+		    if ev.Mask&inotify.IN_MODIFY != inotify.IN_MODIFY {
+			    continue
+		    }
+		    log.Println("Reloading configuration")
+            case err := <-watcher.Error:
+                log.Println("error:", err)
+            }
+        }
+    }()
+
+    err = watcher.Watch("/mnt/LONTAS/ExpControl/dire15/info")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Hang so program doesn't exit
+    <-done
+    /* ... do stuff ... */
+    watcher.Close()
+}
