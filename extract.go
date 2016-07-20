a69e3bb3fac41d40dc7d6cfd6b3279dc5276e901
diliprenkila <dilip.renkila278@gmail.com>
2016-07-20 16:04:56 +0200

    new go package

--- extract.go
+++ extract.go
@@ -8,7 +8,7 @@ import (
 	"regexp"
 	"strings"
 	"strconv"
-	"os/exec"
+
 )
 
 func read_file (path string) ([]string, error) {
@@ -40,72 +40,12 @@ func append_file(lines string) error {
 
 	return err
 }
-func printCommand(cmd *exec.Cmd) {
-  fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
-}
 
-func printError(err error) {
-  if err != nil {
-    os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
-  }
-}
-
-func printOutput(outs []byte,filename string) {
-  if len(outs) > 0 {
- //   fmt.Printf("==> Output: %s\n", string(outs))
-	//  pwd := "/home/ats/dire15/thesis/logs"
-	  file, err := os.OpenFile(
-         filename,
-        os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
-        0666,
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-
-    // Write bytes to file
-    bytesWritten, err := file.Write(outs)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Printf("Wrote %d bytes to %s .\n", bytesWritten,filename)
-
-  }
-}
 
 func main() {
 
 
-	delay,err := read_file("/delay.txt")
-	if err != nil {
-		log.Fatalf("read_file: %s",err)
-	}
-
-	expid,err := read_file("/expid/expid.txt")
-	if err != nil {
-		log.Fatalf("read_file: %s",err)
-	}
-
-	runid,err := read_file("/runid.txt")
-	if err != nil {
-		log.Fatalf("read_file: %s",err)
-	}
-
-	//converting tracefile to a text file
-	//tracefile := fmt.Sprintf("/mnt/LONTAS/traces/trace-%s-%s.cap",expid[0],runid[0])
-	//tracedestiny := fmt.Sprintf("/home/ats/dire15/thesis/logs/trace-%s-%s.txt",expid[0],runid[0])
-	//cmd :=exec.Command("capshow",tracefile)
-	// Create an *exec.Cmd for executing os commands
-	// Combine stdout and stderr
-	//printCommand(cmd)
-	//output, err := cmd.CombinedOutput()
-	//printError(err)
-
-	//printOutput(output,tracedestiny)
-
-
-	lines ,err := read_file(fmt.Sprintf("/logs/trace-%s-%s.txt",expid[0],runid[0]))
+	lines ,err := read_file(fmt.Sprintf("/logs/trace-7541-1.txt")
 	if err != nil {
 		log.Fatalf("read_file: %s", err)
 	}
@@ -138,6 +78,7 @@ func main() {
 
 	for i, _ := range lines_d01 {
 		in := strings.Split(lines_d01[i], ":")
+		fmt.Println(in)
 		d01_checksum_string := strings.Split(lines_d01[i], "=")
 		In, _ := strconv.ParseFloat(in[3], 64)
 		checksum := d01_checksum_string[2]
@@ -168,9 +109,9 @@ func main() {
 	fmt.Println(delay_in_ms,drop)
 
 
-	err = append_file(fmt.Sprintf("expid:%s runid:%s delay:%s average_delay:%f dropped_packets:%d\n",expid[0],runid[0],delay[0],delay_in_ms,drop))
-	if err != nil {
-		log.Fatalf("append_file: %s",err)
+//	err = append_file(fmt.Sprintf("expid:%s runid:%s delay:%s average_delay:%f dropped_packets:%d\n",expid[0],runid[0],delay[0],delay_in_ms,drop))
+//	if err != nil {
+//		log.Fatalf("append_file: %s",err)
 	}
 
 }
