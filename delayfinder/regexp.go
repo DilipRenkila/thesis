6e9a475c1a127fb1f84d3b9041696915d7e7bf5e
diliprenkila <dilip.renkila278@gmail.com>
2016-07-20 15:14:15 +0200

    new go package

--- delayfinder/regexp.go
+++ delayfinder/regexp.go
@@ -1,12 +1,15 @@
 package main
 
 import ("fmt"
+	"strings"
 )
 
 
 func secondmain() {
 	info , _ := read_file(fmt.Sprintf("/info/details.txt"))
-	fmt.Println(len(info))
-	fmt.Println(info[len(info)-1])
+	infoarray  := strings.Split(info[len(info)-1], " ")
+	exp := strings.Split(infoarray[0], ":")
+	expid := exp[1]
+	fmt.Println(expid)
 	return 
 }
