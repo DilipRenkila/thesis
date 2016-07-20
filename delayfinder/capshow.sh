c7dfc3b73ec58757a33525ebca0b72d752bc5553
diliprenkila <dilip.renkila278@gmail.com>
2016-07-20 11:04:12 +0200

    new go package

--- delayfinder/capshow.sh
+++ delayfinder/capshow.sh
@@ -2,5 +2,6 @@
 input="$(tail -1 /mnt/LONTAS/ExpControl/dire15//info/details.txt)"
 set -f
 array=(${input// / })
-echo "${array[0]}"
+exp=(${array[0]//:/ })
+echo "${exp[1]}"
 #capshow /mnt/LONTAS/traces/trace-7520-1.cap >> /mnt/LONTAS/ExpControl/dire15/logs/trace-7518-1.txt
