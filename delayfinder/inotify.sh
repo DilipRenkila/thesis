041e799c70cb6438d63051c418aaa22144853eee
diliprenkila <dilip.renkila278@gmail.com>
2016-07-20 11:17:24 +0200

    new go package

--- delayfinder/inotify.sh
+++ delayfinder/inotify.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 inotifywait -q -m -e close_write /mnt/LONTAS/ExpControl/dire15//info/details.txt |
 while read -r filename event; do
+  sleep 120
   ./capshow.sh         # or "./$filename"
 done
