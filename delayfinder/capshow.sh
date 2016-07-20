2f82f03849352e1312585c00c46a4c0f7bc43a62
diliprenkila <dilip.renkila278@gmail.com>
2016-07-20 10:56:47 +0200

    new go package

--- delayfinder/capshow.sh
+++ delayfinder/capshow.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-input="/mnt/LONTAS/ExpControl/dire15/info/details.txt"
-while IFS= read -r var
-do
-    echo "$var"
-done < "$input"
-echo "$var"
+input="tail -1 /mnt/LONTAS/ExpControl/dire15//info/details.txt "
+echo "$input"
 #capshow /mnt/LONTAS/traces/trace-7520-1.cap >> /mnt/LONTAS/ExpControl/dire15/logs/trace-7518-1.txt
