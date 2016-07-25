b972a751d00266504393c6af70b61d0e5034faf7
diliprenkila <dilip.renkila278@gmail.com>
2016-07-25 10:56:12 +0200

    fix the go regular expression file

--- DelayFinder/inotify.sh
+++ DelayFinder/inotify.sh
@@ -3,5 +3,5 @@ inotifywait -q -m -e close_write /mnt/LONTAS/ExpControl/dire15/status/status |
 while read -r filename event; do
   echo "sleeping for 150 sec"
   sleep 150
-  bash onewaydelay.sh
+  bash /home/ats/dire15/thesis/DelayFinder/onewaydelay.sh
 done
