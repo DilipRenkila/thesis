fd1b9eaceb5ad5993ebe1233a6202da903066fa4
diliprenkila <dilip.renkila278@gmail.com>
2016-07-30 00:07:53 +0200

    fix the go regular expression file

--- DelayFinder/inotify.sh
+++ DelayFinder/inotify.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-inotifywait -q -m -e close_write /mnt/LONTAS/ExpControl/dire15/status/status |
-while read -r filename event; do
-  echo "sleeping for 150 sec"
-
-  sleep 150
-  echo "Hi"
-  bash /home/ats/dire15/thesis/DelayFinder/onewaydelay.sh
-  /home/ats/dire15/thesis/DelayFinder/DelayFinder
+inotifywait -m /mnt/LONTAS/traces/ -e create -e moved_to |
+while read /mnt/LONTAS/traces/ action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
 done
 wait
