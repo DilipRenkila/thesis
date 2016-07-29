f8afdb8d4de52b54e1a1925569b43cf90af081ca
diliprenkila <dilip.renkila278@gmail.com>
2016-07-30 00:11:33 +0200

    fix the go regular expression file

--- DelayFinder/inotify.sh
+++ DelayFinder/inotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 inotifywait -m /mnt/LONTAS/traces -e create -e moved_to |
-while read /mnt/LONTAS/traces action file; do
+while read action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
 done
 wait
