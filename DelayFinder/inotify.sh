6756eba7b6d15651f316a7aba65bab43a9e603aa
diliprenkila <dilip.renkila278@gmail.com>
2016-07-30 12:54:26 +0200

    fix the go regular expression file

--- DelayFinder/inotify.sh
+++ DelayFinder/inotify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MONITORDIR="/mnt/LONTAS/ExpControl/dire15/logs/"
+MONITORDIR="/mnt/LONTAS/traces/"
 inotifywait -m -e create --format '%w%f' "${MONITORDIR}"   |
 while read file; do
         echo "The file '$file' appeared in directory "
