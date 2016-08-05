76e24cba682b90086f209d48acfcd0a1d5d338dd
diliprenkila <dilip.renkila278@gmail.com>
2016-08-05 22:50:03 +0200

    fix the go regular expression file

--- snmp_processor/file_read.go
+++ snmp_processor/file_read.go
@@ -19,7 +19,7 @@ func read_file () ([]string, error) {
 		var current_line []string
 		current_line = append(current_line,scanner.Text())
 		infoarray  := strings.Split(current_line[0], " ")
-		fmt.Println(infoarray[0])
+		fmt.Println(infoarray[9])
 //		status := strings.Split(infoarray[9], ":")
 //		if status[1] == "pending" {
 			lines = append(lines, scanner.Text())
