03a51bde400ce052d2b83ea69d596b0e967d9099
diliprenkila <dilip.renkila278@gmail.com>
2016-08-05 22:35:06 +0200

    fix the go regular expression file

--- snmp_processor/file_read.go
+++ snmp_processor/file_read.go
@@ -2,7 +2,7 @@ package main
 
 import "os"
 import "bufio"
-//import "strings"
+import "strings"
 import "fmt"
 
 func read_file () ([]string, error) {
@@ -16,8 +16,9 @@ func read_file () ([]string, error) {
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-	//	fmt.Println(scanner.Text())
-//		infoarray  := strings.Split(scanner.Text(), " ")
+	//
+		infoarray  := strings.Split(scanner.Text(), " ")
+		fmt.Println(infoarray[8])
 //		status := strings.Split(infoarray[9], ":")
 //		if status[1] == "pending" {
 			lines = append(lines, scanner.Text())
