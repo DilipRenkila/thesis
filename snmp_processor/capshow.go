bca617cd44a2d822455895befc1c536151bb72c1
diliprenkila <dilip.renkila278@gmail.com>
2016-08-07 22:55:58 +0200

    capshow

--- snmp_processor/capshow.go
+++ snmp_processor/capshow.go
@@ -1,22 +1,57 @@
 package main
 
 import "os/exec"
-import "fmt"
-
-func capshows() error {
-	//cmd := "/usr/local/bin/capshow"
-	//tracefile := fmt.Sprintf("/mnt/LONTAS/traces/trace-%d-%d.cap",expid,runid)
-	//tracedest := fmt.Sprintf("/mnt/LONTAS/ExpControl/dire15/logs/trace-%d-%d.trace",expid,runid)
-	//args := []string{tracefile}
-	if err := exec.Command("capshow","/mnt/LONTAS/traces/trace-7754-1.cap").Run(); err != nil {
-		return err
-	}
-	return nil
+import (
+	"fmt"
+	"os"
+)
+import "strings"
+import "log"
+func printCommand(cmd *exec.Cmd) {
+  fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
 }
-func main(){
-	var err error
-	err = capshows()
-	if err != nil {
-		fmt.Println(err)
-	}
+
+func printError(err error) {
+  if err != nil {
+    os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+  }
+}
+
+func printOutput(outs []byte,filename string) {
+  if len(outs) > 0 {
+ //   fmt.Printf("==> Output: %s\n", string(outs))
+	//  pwd := "/home/ats/dire15/thesis/logs"
+	  file, err := os.OpenFile(
+         filename,
+        os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+        0666,
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    // Write bytes to file
+    bytesWritten, err := file.Write(outs)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("Wrote %d bytes to %s .\n", bytesWritten,filename)
+
+  }
+}
+
+func main() {
+
+	//converting tracefile to a text file
+	tracefile := fmt.Sprintf("/mnt/LONTAS/traces/trace-7754-1.cap")
+	tracedestiny := fmt.Sprintf("/home/ats/dire15/thesis/logs/trace-7754-1.txt")
+	cmd := exec.Command("capshow", tracefile)
+	// Create an *exec.Cmd for executing os commands
+	// Combine stdout and stderr
+	printCommand(cmd)
+	output, err := cmd.CombinedOutput()
+	printError(err)
+	printOutput(output, tracedestiny)
+
 }
