69d330d25b87039a89069d5ab1218a0d22b4cd43
diliprenkila <dilip.renkila278@gmail.com>
2016-08-07 14:54:58 +0200

    checking startup.sh file

--- snmp_processor/mysql.go
+++ snmp_processor/mysql.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    _ "github.com/go-sql-driver/mysql"
-    "database/sql"
-    "fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"time"
 )
 
 func main() {
@@ -19,7 +20,11 @@ func main() {
 		var delay_on_shaper,packet_distribution,interframegap_distribution,destination string
 		err = rows.Scan(&expid,&runid,&keyid,&delay_on_shaper,&packets_sent,&min_packet_length,&max_packet_lenth,&packet_distribution,&sampling_interval,&min_intergramegap,&max_intergramegap,&interframegap_distribution,&destination,&status,&when_to_process)
 		checkErr(err)
-		fmt.Println(when_to_process)
+		unixtime := int64(time.Now().Unix())
+		if (unixtime > when_to_process ){
+			fmt.Println("ok, you can process")
+		}
+
 	}
 
 	db.Close()
