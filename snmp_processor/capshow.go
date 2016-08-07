d97d88d75bcae8ae65568a61d3bc9c67f8048522
diliprenkila <dilip.renkila278@gmail.com>
2016-08-07 22:51:39 +0200

    capshow

--- snmp_processor/capshow.go
+++ snmp_processor/capshow.go
@@ -3,19 +3,19 @@ package main
 import "os/exec"
 import "fmt"
 
-func capshows(expid int,runid int) error {
+func capshows() error {
 	//cmd := "/usr/local/bin/capshow"
-	tracefile := fmt.Sprintf("/mnt/LONTAS/traces/trace-%d-%d.cap",expid,runid)
+	//tracefile := fmt.Sprintf("/mnt/LONTAS/traces/trace-%d-%d.cap",expid,runid)
 	//tracedest := fmt.Sprintf("/mnt/LONTAS/ExpControl/dire15/logs/trace-%d-%d.trace",expid,runid)
 	//args := []string{tracefile}
-	if err := exec.Command("capshow",tracefile).Run(); err != nil {
+	if err := exec.Command("capshow","/mnt/LONTAS/traces/trace-7754-1.cap").Run(); err != nil {
 		return err
 	}
 	return nil
 }
 func main(){
 	var err error
-	err = capshows(7754,1)
+	err = capshows()
 	if err != nil {
 		fmt.Println(err)
 	}
