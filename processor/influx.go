ee7a3182b9003bf5cab6a30b185c8ec06a0c4fb0
diliprenkila <dilip.renkila278@gmail.com>
2016-08-10 19:58:26 +0200

    returning string arrays in regexp.go

--- processor/influx.go
+++ processor/influx.go
@@ -28,7 +28,7 @@ func ExampleClient_Query() {
 	}
 	if response, err := con.Query(q); err == nil && response.Error() == nil {
 		fmt.Println(reflect.TypeOf(response.Results[0].Series[0].Values[0][1]))
-		for i := 0;i < len(response.Results[0].Series[0].Values) - 1; i ++ {
+		for i := 0;i < len(response.Results[0].Series[0].Values) ; i ++ {
 			length := response.Results[0].Series[0].Values[0][i]
 			fmt.Println(reflect.TypeOf(length))
 			//value,_ := strconv.ParseInt(string,10,64)
