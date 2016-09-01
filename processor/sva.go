be324f7b1d0837739ffb410262ced65e66c3ffa3
diliprenkila <dilip.renkila278@gmail.com>
2016-09-01 14:28:29 +0200

    new logic for X[i]

--- processor/sva.go
+++ processor/sva.go
@@ -77,7 +77,7 @@ func sva(expid int,runid int,intime time.Time) error {
 
 	for i := 0; i < len(in_uptime)-1; i++ {
 		inbitrate=append(inbitrate,float64(Bytes_in[i+1]-Bytes_in[i])/(in_uptime[i+1]-in_uptime[i]))
-		outbitrate=append(outbitrate,float64(Bytes_out[i+1]-Bytes_out[i])/(out_uptime[i+1]-out_uptime[i]))
+		//outbitrate=append(outbitrate,float64(Bytes_out[i+1]-Bytes_out[i])/(out_uptime[i+1]-out_uptime[i]))
 		interval := in_uptime[i+1]-in_uptime[i]
 		outtime := timemachine(intime,interval)
 		str:=fmt.Sprintf("select * from in_%d_%d where time > %v and time < %v",expid,runid,intime.UnixNano(),outtime.UnixNano())
@@ -91,7 +91,6 @@ func sva(expid int,runid int,intime time.Time) error {
 		} else {
 			X=append(X,X[i-1]+float64(size-size1))
 		}
-		fmt.Println(inbitrate)
 		fmt.Println(fmt.Sprintf("interval:%d,time_range:%v-%v,bytes_src:%d,bytes_dest:%d,inbitrate:%f,outbitrate:%f,aditional_delay:%f", i+1,intime.UnixNano(),outtime.UnixNano(),size,size1,inbitrate[i],outbitrate[i],X[i]/inbitrate[i]))
 
 		intime = outtime
