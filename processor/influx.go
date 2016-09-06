b3c93d788f3925ab78f99098f437a6ba9f30eb9b
diliprenkila <dilip.renkila278@gmail.com>
2016-09-06 07:31:23 +0200

    checking the sample size

--- processor/influx.go
+++ processor/influx.go
@@ -78,7 +78,7 @@ func Influx_Write(d []string,d_l []string,tablename string) (time.Time,error)  {
 		decimal_part, _ := strconv.ParseInt(timestring[1], 10, 64)
 		value,_ := strconv.ParseInt(d_l[i],10,64)
 		unixtime := time.Unix(integer_part,decimal_part)
-		fmt.Println(d[i])
+
 		pts[i] = client.Point{
 			Measurement: tablename,
 			Fields: map[string]interface{}{
@@ -102,5 +102,6 @@ func Influx_Write(d []string,d_l []string,tablename string) (time.Time,error)  {
 	integer_part, _ := strconv.ParseInt(timestring[0], 10, 64)
 	decimal_part, _ := strconv.ParseInt(timestring[1], 10, 64)
 	firsttime = time.Unix(integer_part,decimal_part)
+			fmt.Println(d[0])
 	return firsttime,nil
 }
